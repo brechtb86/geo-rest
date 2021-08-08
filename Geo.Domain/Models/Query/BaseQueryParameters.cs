3ee759fdf50af33d88265203b798d525f5abb5bd
Brecht Baekelandt <brecht.baekelandt@outlook.com>
2021-08-08 22:29:17 +0200

    Documentation.

--- Geo.Domain/Models/Query/BaseQueryParameters.cs
+++ Geo.Domain/Models/Query/BaseQueryParameters.cs
@@ -9,7 +9,7 @@ namespace Geo.Rest.Domain.Models.Query
     public abstract class BaseQueryParameters
     {
         /// <summary>
-        /// A pipe-seperated list of the fields that should be returned.
+        /// A pipe-seperated list of the fields that should be returned (e.g. name|id|createdAt).
         /// </summary>
         /// <value>
         /// The fields.
